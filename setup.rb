d1d99c1a4c16d8d3e01bf38c8f1d1d7c96c3871c
EmanuelN <emanuelneves10@gmail.com>
2017-08-29 12:32:00 -0400

    added env file

--- setup.rb
+++ setup.rb
@@ -10,8 +10,8 @@ puts 'Establishing connection to database ...'
 ActiveRecord::Base.establish_connection(
   adapter: 'postgresql',
   database: 'ar_exercises',
-  username: 'development',
-  password: 'development',
+  username: ENV['USER_NAME'],
+  password: ENV['PASSWORD'],
   host: 'localhost',
   port: 5432,
   pool: 5,
