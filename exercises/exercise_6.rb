9565a643b0aadf15afd72baa3a0555ab59f4b5a1
EmanuelN <emanuelneves10@gmail.com>
2017-08-29 16:35:32 -0400

    exercise 8: passwords

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -11,10 +11,20 @@ puts "----------"
 # Your code goes here ...
 class Employee < ActiveRecord::Base
   belongs_to :store
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates_inclusion_of :hourly_rate, :in => 40..200
   validates_presence_of :store
+
+  before_save :random_password
+
+  private
+
+  def random_password
+    self.password = (0...8).map { (33 + rand(60)).chr }.join
+  end
+
 end
 
 @store1.employees.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, hourly_rate: 60)
